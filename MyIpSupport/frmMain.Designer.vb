a607f10ee2c8205de1502e61a4362464e35cd75b
meamod <james@fe0.uk>
2015-02-19 17:47:42 +0000

    Commit for old project

---
+++ MyIpSupport/frmMain.Designer.vb
@@ -0,0 +1,116 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblIP = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.BackColor = System.Drawing.Color.White
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Controls.Add(Me.btnClose)
+        Me.Panel1.Location = New System.Drawing.Point(-4, 132)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(383, 47)
+        Me.Panel1.TabIndex = 0
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox1.Image = Global.MyIpSupport.My.Resources.Resources.spot2
+        Me.PictureBox1.Location = New System.Drawing.Point(-1, 3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 36)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(290, 11)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 1
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(1, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(277, 37)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Your Local IP Address:"
+        '
+        'lblIP
+        '
+        Me.lblIP.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblIP.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblIP.Location = New System.Drawing.Point(-4, 57)
+        Me.lblIP.Name = "lblIP"
+        Me.lblIP.Size = New System.Drawing.Size(383, 46)
+        Me.lblIP.TabIndex = 2
+        Me.lblIP.Text = "000.000.000.000"
+        Me.lblIP.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(374, 177)
+        Me.Controls.Add(Me.lblIP)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "MyIpSupport"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents btnClose As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblIP As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ MyIpSupport/frmMain.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+
+Public Class frmMain
+    Private Sub btnClose_Click (ByVal sender As Object, ByVal e As EventArgs) Handles btnClose.Click
+        Application.Exit()
+    End Sub
+
+    Private Function IPAddress() As String
+        Dim host As IPHostEntry
+        Dim localIP As String = "Error"
+        host = Dns.GetHostEntry (Dns.GetHostName())
+        For Each ip As IPAddress In host.AddressList
+            If ip.AddressFamily = AddressFamily.InterNetwork Then
+                'Console.WriteLine (ip.ToString())
+                If ip.ToString = "127.0.0.1" Then
+                    localIP = "Loopback Error"
+                Else
+                    localIP = ip.ToString()
+                    Exit For
+                End If
+            End If
+        Next
+        Return localIP
+    End Function
+
+    Private Sub frmMain_Load (ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        lblIP.Text = IPAddress()
+    End Sub
+
+    Private Sub PictureBox1_Click (ByVal sender As Object, ByVal e As EventArgs) Handles PictureBox1.Click
+        Process.Start ("http://meamod.com")
+    End Sub
+End Class
